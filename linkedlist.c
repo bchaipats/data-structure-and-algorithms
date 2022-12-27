8a7409bebcb777b2e2984ac45c4b30add7bb71c3
Chaipat Suwannapoom <bchaipats@gmail.com>
2022-12-27 18:51:04 +0700

    initial commit

---
+++ linkedlist.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void Insert(struct Node** head, int x) {
+    struct Node* temp = malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->next = *head;
+    *head = temp;
+}
+
+void Print(struct Node* head) {
+    printf("List is: ");
+    while (head != NULL) {
+        printf(" %d", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    int n, i, x;
+
+    printf("How many numbers?\n");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        printf("Enter the number\n");
+        scanf("%d", &x);
+        Insert(&head, x);
+        Print(head);
+    }
+}
